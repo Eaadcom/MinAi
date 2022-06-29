d9c598aef02df0f2fa270509fec58cc28ba42cb4
edwarddeen <eaadcom@gmail.com>
2022-06-29 15:38:53 +0200

    Create Branch without results

--- Assets/SharedAssets/Alles/testomgeving/scripts/spawnobject.cs
+++ Assets/SharedAssets/Alles/testomgeving/scripts/spawnobject.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 namespace bigArena
 {
@@ -16,6 +18,7 @@ namespace bigArena
         private Vector3 size;
         public int WallsCount;
         public List<GameObject> CloneWalls = new List<GameObject>();
+        private float curriculumLesson;
 
 
         // Start is called before the first frame update
@@ -29,6 +32,7 @@ namespace bigArena
             //center = platform.transform.position;
             //center.y += 0.8f;*/
             float arenaScale = Academy.Instance.EnvironmentParameters.GetWithDefault("arenaScale", 1f);
+            this.curriculumLesson = Academy.Instance.EnvironmentParameters.GetWithDefault("curriculumLesson", 1f);
             platform.transform.parent.transform.localScale = new Vector3(arenaScale, 1, arenaScale);
             size = platform.transform.localScale * platform.transform.parent.transform.localScale.z;
             center = platform.transform.position;
@@ -39,13 +43,13 @@ namespace bigArena
         public void ResetArena()
         {
             float arenaScale = Academy.Instance.EnvironmentParameters.GetWithDefault("arenaScale", 1f);
+            this.curriculumLesson = Academy.Instance.EnvironmentParameters.GetWithDefault("curriculumLesson", 1f);
             platform.transform.parent.transform.localScale = new Vector3(arenaScale, 1, arenaScale);
             size = platform.transform.localScale * platform.transform.parent.transform.localScale.z;
             center = platform.transform.position;
             center.y += 0.8f;
 
-            SpawnWall();
-            SpawnGoal();
+            ArenaManager();
         }
 
         // Update is called once per frame
@@ -54,8 +58,69 @@ namespace bigArena
             /* if(Input.GetKey(KeyCode.T))
              SpawnWall();*/
         }
+
+        public void ArenaManager()
+        {
+            WallsCount = 0;
+
+            if (curriculumLesson == 1.0)
+            {
+                SpawnGoal();
+            }
+
+            if (curriculumLesson == 2.0)
+            {
+                SpawnGoal();
+            }
+
+            if (curriculumLesson == 3.0)
+            {
+                SpawnGoal();
+            }
+            
+            if (curriculumLesson == 4.0)
+            {
+                SpawnGoal();
+            }
+            
+            if (curriculumLesson == 5.0)
+            {
+                SpawnLessonOneWall();
+                SpawnLessonOneGoal();
+            }
+            
+            if (curriculumLesson == 6.0)
+            {
+                SpawnLessonOneWall();
+                SpawnLessonTwoGoal();
+            }
+        }
+
+        public void SpawnLessonZeroGoal()
+        {
+            Goalprefab.transform.position = center + new Vector3(25, 0, 20);
+        }
+
+        public void SpawnLessonTwoWall()
+        {
+            Vector3 pos = center + new Vector3(0, 0 ,-20);
+            Transform WallClone = Instantiate(Wallprefab, pos, Quaternion.Euler(0, 0, 0));
+            WallClone.localScale = new Vector3(WallClone.localScale.x, Random.Range(2f, 12f),
+                WallClone.localScale.z);
+            CloneWalls.Add(WallClone.gameObject);
+        }
+
+        public void SpawnLessonOneWall()
+        {
+            Vector3 pos = center + new Vector3(0, 0 ,35);
+            Transform WallClone = Instantiate(Wallprefab, pos, Quaternion.Euler(0, 0, 0));
+            WallClone.localScale = new Vector3(WallClone.localScale.x, Random.Range(2f, 12f),
+                WallClone.localScale.z);
+            CloneWalls.Add(WallClone.gameObject);
+        }
         public void SpawnWall()
         {
+            
             for (int i = 0; i < WallsCount; i++)
             {
 
@@ -65,13 +130,33 @@ namespace bigArena
                 {
                     Transform WallClone = Instantiate(Wallprefab, pos, Quaternion.Euler(0, Random.Range(0, 360), 0));
                     //This changes the height of the instantiated clone randomly.
-                    WallClone.localScale = new Vector3(WallClone.localScale.x ,Random.Range(2f, 12f), WallClone.localScale.z);
+                    WallClone.localScale = new Vector3(WallClone.localScale.x, Random.Range(2f, 12f),
+                        WallClone.localScale.z);
                     CloneWalls.Add(WallClone.gameObject);
                 }
 
                 //CloneWalls.AddRange(GameObject.FindGameObjectsWithTag("wall"));
-                
+
             }
+        }
+        
+        public void SpawnLessonOneGoal()
+        {
+            Goalprefab.transform.position = center + new Vector3(30, 0, 55);
+        }
+        
+        public void SpawnLessonTwoGoal()
+        {
+            
+            Goalprefab.transform.position = center + new Vector3(0, 0, 55);
+            // if (Random.Range(1, 3) == 1)
+            // {
+            //     Goalprefab.transform.position = center + new Vector3(0, 0, 30);
+            // }
+            // else
+            // {
+            //     Goalprefab.transform.position = center + new Vector3(0, 0, -30);
+            // }
         }
 
         public void SpawnGoal()
